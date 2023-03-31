097f16fa3e71644c38e0887f3c011f7ad309a988
chsts0 <staschekalin5@gmail.com>
2023-03-31 14:02:57 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+function changeOpacity() {
+	var soup_1st = document.getElementById("soup_1st");
+	var soup_2nd = document.getElementById("soup_2nd");
+	var name_1st_soup = document.getElementById("name_1st_soup");
+	var name_2nd_soup = document.getElementById("name_2nd_soup");
+
+	soup_1st.style.opacity = "0";
+	name_1st_soup.style.opacity = "0";
+	soup_2nd.style.opacity = "1";
+	name_2nd_soup.style.opacity = "1";
+}
+
+function changeOpacity1() {
+	var soup_1st = document.getElementById("soup_1st");
+	var soup_2nd = document.getElementById("soup_2nd");
+	var name_1st_soup = document.getElementById("name_1st_soup");
+	var name_2nd_soup = document.getElementById("name_2nd_soup");
+
+	soup_1st.style.opacity = "1";
+	name_1st_soup.style.opacity = "1";
+	soup_2nd.style.opacity = "0";
+	name_2nd_soup.style.opacity = "0";
+}
+
+function changeOpacity2() {
+	var burger = document.getElementById("burger");
+	var stake = document.getElementById("stake");
+	var name_burger = document.getElementById("name_burger");
+	var name_stake = document.getElementById("name_stake");
+
+	burger.style.opacity = "0";
+	name_burger.style.opacity = "0";
+	stake.style.opacity = "1";
+	name_stake.style.opacity = "1";
+}
+
+function changeOpacity3() {
+	var burger = document.getElementById("burger");
+	var stake = document.getElementById("stake");
+	var name_burger = document.getElementById("name_burger");
+	var name_stake = document.getElementById("name_stake");
+
+	burger.style.opacity = "1";
+	name_burger.style.opacity = "1";
+	stake.style.opacity = "0";
+	name_stake.style.opacity = "0";
+}
+
+function changeOpacity4() {
+	var rain = document.getElementById("rain");
+	var toxic = document.getElementById("toxic");
+	var name_rain = document.getElementById("name_rain");
+	var name_toxic = document.getElementById("name_toxic");
+
+	rain.style.opacity = "0";
+	name_rain.style.opacity = "0";
+	toxic.style.opacity = "1";
+	name_toxic.style.opacity = "1";
+}
+
+function changeOpacity5() {
+	var rain = document.getElementById("rain");
+	var toxic = document.getElementById("toxic");
+	var name_rain = document.getElementById("name_rain");
+	var name_toxic = document.getElementById("name_toxic");
+
+	rain.style.opacity = "1";
+	name_rain.style.opacity = "1";
+	toxic.style.opacity = "0";
+	name_toxic.style.opacity = "0";
+}
